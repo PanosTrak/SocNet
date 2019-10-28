1923d5684de22cb1832b8fd5ceb13c6ac8cfadc9
PanosTrak <pnstrak@gmail.com>
2019-10-28 03:22:30 +0200

    Changes to models.py

--- main/models.py
+++ main/models.py
@@ -1,13 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
 
-class Account(User):
+class Account(AbstractUser):
     date_born = models.DateField(null=True)
 
+
 class Profile(models.Model):
-    uid = models.IntegerField(primary_key=True)
-    account_id = models.OneToOneField(Account, on_delete=models.SET_NULL, null=True)
-    picture = models.ImageField()
-    bio = models.TextField()
+    bio = models.TextField(max_length=200)
+    account = models.OneToOneField(
+        to=Account, on_delete=models.SET_NULL, null=True)
+
+
+class FriendList(models.Model):
+    account_1 = models.ForeignKey(
+        'Profile', on_delete=models.CASCADE, related_name='account_1')
+    account_2 = models.ForeignKey(
+        'Profile', on_delete=models.CASCADE, related_name='account_2')
